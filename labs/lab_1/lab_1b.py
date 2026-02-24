416473933875af37c96101975ca052e2b7720a94
codeCrafter10-alt <aaravg1410@gmail.com>
2026-02-24 10:29:38 -0800

    Added user input validation for operation
    Added function to sanitize and request valid operation from user.

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -9,9 +9,24 @@ and prints the result to the terminal window.
 
 """
 
-def sanitize_input(prompt: str) -> float:
+def validate_operation(prompt: str) -> str:
     """
-    Function to request and santize user input for the operation
+    Function to request valid operation from the user
+
+    Returns:
+        str: the operation in lowercase
+    """
+
+    while True:
+        operation = input(prompt).strip().lower()
+        if operation not in ["add", "subtract", "multiply", "divide"]:
+            print("Invalid input. Please enter a valid operation.")
+        else:
+            return operation
+
+def sanitize_number_input(prompt: str) -> float:
+    """
+    Function to request and santize user input for the numbers
 
     Returns:
         float: The sanitized numeric input by the user
@@ -49,17 +64,15 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             return num1 / num2
         else:
             raise ValueError("Cannot divide by zero.")
-    else:
-        raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = sanitize_input("Enter the first number: ")
-    num2 = sanitize_input("Enter the second number: ")
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    num1 = sanitize_number_input("Enter the first number: ")
+    num2 = sanitize_number_input("Enter the second number: ")
+    operation = validate_operation("Enter the operation (add, subtract, multiply, divide): ")
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
